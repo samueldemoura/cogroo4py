e6978bd280ace997ad37d9f18734aadc624ed0d1
Márcio Monteiro <muriloconte@gmail.com>
2018-06-01 17:18:14 -0300

    Criando ambiente maven para compilar o gateway.

---
+++ cogroo4py/src/test/java/br/edu/univali/lia/cogroo/CogrooPythonInterfaceTest.java
@@ -0,0 +1,38 @@
+package br.edu.univali.lia.cogroo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CogrooPythonInterfaceTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CogrooPythonInterfaceTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CogrooPythonInterfaceTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
