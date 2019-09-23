13f03ec4cde0ee30a04bd0bcdaca687eae69c219
Samuel de Moura <samueldemouramoreira@gmail.com>
2019-09-23 19:51:32 -0300

    Remove py4j dependency in Java project

--- cogroo4py/src/main/java/br/edu/univali/lia/cogroo/CogrooPythonInterface.java
+++ cogroo4py/src/main/java/br/edu/univali/lia/cogroo/CogrooPythonInterface.java
@@ -12,44 +12,35 @@ import org.cogroo.checker.GrammarChecker;
 import org.cogroo.text.Document;
 import org.cogroo.text.impl.DocumentImpl;
 
-import py4j.GatewayServer;
-
 public class CogrooPythonInterface {
-	
-	public ComponentFactory factory = ComponentFactory.create(new Locale("pt", "BR"));
+
+	public ComponentFactory factory;
 	public Analyzer cogroo;
 	public GrammarChecker gc;
-	
-	public static void main(String[] args) throws IllegalArgumentException, IOException {		
-		CogrooPythonInterface app = new CogrooPythonInterface();
-	    GatewayServer server = new GatewayServer(app);
-	    server.start();	
-	    System.out.println("Gateway Server Started");	   	   
-	}
-	
+
 	public CogrooPythonInterface() throws IllegalArgumentException, IOException {
-		cogroo = factory.createPipe();		
+		factory = ComponentFactory.create(new Locale("pt", "BR"));
+		cogroo = factory.createPipe();
 		gc = new GrammarChecker(cogroo);
+
 		Logger globalLogger = Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
 		globalLogger.setLevel(Level.OFF);
 		Logger cogrooLogger = Logger.getLogger("org.cogroo.interpreters.FlorestaTagInterpreter");
-		cogrooLogger.setLevel(Level.OFF);	
-		Logger.getRootLogger().setLevel(Level.OFF); 
+		cogrooLogger.setLevel(Level.OFF);
+		Logger.getRootLogger().setLevel(Level.OFF);
 	}
-	
+
 	public Document analyze(String text) {
-		System.out.println("Text received for analysis: " + text);
 		DocumentImpl doc = new DocumentImpl();
-		doc.setText(text);		
+		doc.setText(text);
 		cogroo.analyze(doc);
 		return doc;
 	}
-	
+
 	public CheckDocument grammarCheck(String text) {
-		System.out.println("Text received for analysis: " + text);
 		CheckDocument doc = new CheckDocument(text);
 		gc.analyze(doc);
 		return doc;
-	}	
-	
+	}
+
 }
