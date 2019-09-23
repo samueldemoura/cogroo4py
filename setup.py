8f9ddd0f8661a8d8f957992c95d8cf871a86b361
Samuel de Moura <samueldemouramoreira@gmail.com>
2019-09-23 19:53:35 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name='cogroo_interface',
-    version='0.2',
-    description='Interface for accessing CoGrOO from Python scripts using py4j',
-    author='Guilherme Passero',
-    author_email='guilherme.passero0@gmail.com',
-    url='https://github.com/gpassero/cogroo4py',
-    py_modules=['cogroo_interface']
+    name='cogroo4py',
+    version='1.0.0',
+    description='Interface for accessing CoGrOO from Python scripts using pyjnius',
+    author='Samuel de Moura Moreira',
+    author_email='samueldemouramoreira@gmail.com',
+    url='https://github.com/samueldemoura/cogroo4py',
+    py_modules=['cogroo4py']
 )
